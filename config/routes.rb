e74559adb68f5ed9992bfe3d7dad895730405aa7
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-17 15:28:59 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'tents#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :tents, except: [:index] do
+    resources :bookings, only: %i[new create]
   end
+  resources :bookings, only: [:show]
 end
