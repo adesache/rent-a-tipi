65fad696ffe200cdf0356bcbdf1f9e0cf1697bd4
romain barbier <romainbarbier1212@hotmail.com>
2020-02-17 17:11:54 +0100

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :email, presence: true
+  validates_uniqueness_of :first_name, :scope => [:last_name]
   has_one_attached :photo
 end
