cf3a3d12938c0323292fdfa8aacfa86826a1a853
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-17 17:41:05 +0100

    validates ok

--- app/models/tent.rb
+++ app/models/tent.rb
@@ -1,4 +1,8 @@
 class Tent < ApplicationRecord
   belongs_to :user
-  has_many_attached :photo
+  LEVEL = [1,2,3,4,5]
+  has_many_attached :photos
+  validates :title, :description, :capacity, :location, :price, presence: true
+  validates :title, uniqueness: true
+  validates :comfort_level, inclusion: { in: LEVEL }
 end
