cea7697f8e4e3e587f86ce6651f344079ec6c69b
romain barbier <romainbarbier1212@hotmail.com>
2020-02-17 17:41:42 +0100

    solved Devise + changed User validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'tents#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tents, except: [:index]
+  resources :tents
   resources :tents do
     resources :bookings, only: %i[new create]
   end
   resources :bookings, only: [:show]
+
+  root to: 'tents#index'
 end
