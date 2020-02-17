0eaa3e44ff01e9535988ab89319f3d26107ce21f
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-17 17:04:10 +0100

    controller bookings ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @tent = Tent.find(params[:tent_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+     @tent = Tent.find(params[:tent_id])
+     @booking.tent = @tent
+    if @booking.save
+      redirect_to tent_path(@tent)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:tent).permit(:start_date, :end_date, :total_price)
+  end
 end
