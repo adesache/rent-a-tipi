cc89cbb2bef239b4af850b64b811845b9182e56d
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-17 15:35:07 +0100

    modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tents#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tents, except: [:index] do
+  resources :tents, except: [:index]
+  resources :tents do
     resources :bookings, only: %i[new create]
   end
   resources :bookings, only: [:show]
 end
+Collapse
+
+
+
+
+
+
+
+
+
+
+
+
+
