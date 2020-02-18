9b78d778ce6a963391d37f357d7d8bc793fdb309
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-18 14:52:11 +0100

    avatar ok navbar en cours

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,9 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
