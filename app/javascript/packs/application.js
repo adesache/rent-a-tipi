50e098f04dbe72bb2f5a2a7cfbb042f0dd80dcae
romain barbier <romainbarbier1212@hotmail.com>
2020-02-18 15:48:02 +0100

    new tent entry now adds picture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,11 @@
+//= require jquery
+//= require jquery_ujs
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import {} from "jquery-ujs"
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 document.addEventListener('turbolinks:load', () => {
